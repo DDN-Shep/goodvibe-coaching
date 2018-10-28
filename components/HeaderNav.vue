9c14d84472911c1324a49a52e142f5634805dc05
Andrew Sheppard <andrewsheppard@atgmedia.com>
2018-10-28 13:50:23 +0000

    Twenty template wip

---
+++ components/HeaderNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header id="header" class="alt">
+    <nav id="nav">
+      <ul>
+        <li class="current"><a href="index.html">Welcome</a></li>
+        <li class="submenu">
+          <a href="#">Layouts</a>
+          <ul>
+            <li><a href="left-sidebar.html">Left Sidebar</a></li>
+            <li><a href="right-sidebar.html">Right Sidebar</a></li>
+            <li><a href="no-sidebar.html">No Sidebar</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="button primary">Sign Up</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
